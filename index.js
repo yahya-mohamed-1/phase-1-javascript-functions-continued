1701bce25f192040553747aaacbd4266d52a6731
Yahya Mohamed <171671702+yahya-mohamed-1@users.noreply.github.com>
2024-07-03 19:24:29 +0300

    Complete Assignment Submission.

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// code your solution here
+// Function declaration for saturdayFun
+function saturdayFun(activity = "roller-skate") {
+  return `This Saturday, I want to ${activity}!`;
+}
+
+// Function expression for mondayWork
+const mondayWork = function (activity = "go to the office") {
+  return `This Monday, I will ${activity}.`;
+};
+
+// Function that returns a function to wrap an adjective
+function wrapAdjective(flair = "*") {
+  return function (adjective) {
+    return `You are ${flair}${adjective}${flair}!`;
+  };
+}
